990061947c6232554e4d48907bd750062bfcf046
kgmin <cdc.asic@gmail.com>
2023-11-19 18:38:24 +0900

    config

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
 }
 
 module.exports = (_phase, { defaultConfig }) => {
