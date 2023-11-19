52c74f0eae8bd422eec6bc63739c2dac408d58ba
kgmin <cdc.asic@gmail.com>
2023-11-19 16:31:40 +0900

    ?

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import LiveTimestamp from "../LiveTimestamp";
+import LiveTimestamp from "../../app/LiveTimestamp";
 
 type Props = {
   searchParams?: Bundle;
